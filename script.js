a082507266c884137d9fae0ca971a1a87c70842a
yu <134583362+yuw1xx@users.noreply.github.com>
2026-02-12 02:17:26 +0100

    Implement PWA service worker registration
    Added service worker registration and installation prompt handling.

--- script.js
+++ script.js
@@ -619,3 +619,16 @@ function updateDate() {
     { weekday: "long", month: "long", day: "numeric" },
   );
 }
+
+// --- PWA INSTALLATION ---
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+        .then(() => console.log('Service Worker Registered'));
+}
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    console.log("Ready to install!"); 
+})
+  ;
